5d2578bf096956e4f773fb11b21feea84f69e4c8
mianzainch000 <mianzainch000@gmail.com>
2025-07-11 21:58:00 +0500

    ArhamSoft interview

--- src/screens/interviews/index.js
+++ src/screens/interviews/index.js
@@ -47,6 +47,7 @@ const Interview = () => {
         8 sep 2024 : Apps4U Joining 13 sep 2024 : First day apps 4U motor bike:
         5 December 2024 last day
       </div>
+      <div className="container"> 11 july 2025 : ArhamSoft interview</div>
     </div>
   );
 };
